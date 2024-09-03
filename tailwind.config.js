eccf22c159bcd0df8ada5a3546443909cd8ca6b9
Andrew <andrewrgpires@gmail.com>
2024-09-03 09:02:57 -0300

    config colors and fonts tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./**/*.{html,js"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': ['Young-serif'],
+      'p': ['Outfit'],
+    },
+    extend: {
+      colors : {
+        'stone-100': '#F3E6D8',
+        'stone-150': '#E4DED8',
+        'stone-600': '#5F574E',
+        'stone-900': '#302D2C',
+        'brown': '#854632',
+        'rose-50': '#7B284F',
+        'rose-800': '#FFF5FA',
+      },
+    },
   },
   plugins: [],
 }
